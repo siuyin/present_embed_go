a4d68b7d0720c7bd825d74ad21e62ca4c9f11721
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-03 22:03:50 +0800

    feat: got stream processing working

---
+++ 04_stream/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+var (
+	ns *server.Server
+)
+
+func init() {
+	embedNATS()
+}
+
+func main() {
+	go procWorker()
+	http.HandleFunc("/proc", procHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	ns.Shutdown()
+}
+
+func procWorker() {
+	nc, err := nats.Connect("", nats.InProcessServer(ns))
+	if err != nil {
+		log.Fatal("procWorker: Connect: ", err)
+	}
+
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatal("procWorker: new jetstream: ", err)
+	}
+
+	ctx := context.Background()
+	procStrm, err := js.CreateStream(ctx, jetstream.StreamConfig{
+		Name: "proc", Subjects: []string{"proc"},
+		MaxAge: 72 * time.Hour,
+	})
+	if err != nil {
+		js.DeleteStream(ctx, "proc")
+		log.Fatal("procWorker: create stream: ", err)
+	}
+
+	wrkr, err := procStrm.CreateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable: "wrkr", AckPolicy: jetstream.AckExplicitPolicy, AckWait: 2 * time.Second})
+	if err != nil {
+		procStrm.DeleteConsumer(ctx, "wrkr")
+		log.Fatal("procWorker: create consumer: ", err)
+	}
+
+	wrkr.Consume(func(m jetstream.Msg) {
+		fmt.Printf("procWorker: received: %s: ", m.Data())
+		time.Sleep(300 * time.Millisecond)
+		fmt.Printf("processed:  %s\n", m.Data())
+		m.Ack()
+	})
+
+}
+
+func procHandler(w http.ResponseWriter, r *http.Request) {
+	n := r.FormValue("n")
+	id := r.FormValue("id")
+	if n == "" || id == "" {
+		return
+	}
+
+	nn, err := strconv.Atoi(n)
+	if err != nil {
+		return
+	}
+
+	nc, err := nats.Connect("", nats.InProcessServer(ns))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatal("procHandler: new jetstream: ", err)
+	}
+
+	ctx := context.Background()
+	for i := 0; i < nn; i++ {
+		if _, err := js.Publish(ctx, "proc", []byte(fmt.Sprintf("%s:%d", id, i))); err != nil {
+			log.Println("procHandler: publish: ", err)
+		}
+
+	}
+
+	io.WriteString(w, fmt.Sprintf("Request ID: %s: %d processing requests queued.\n", id, nn))
+
+	nc.Flush()
+	nc.Close()
+
+}
+
+func embedNATS() {
+	var err error
+	opts := &server.Options{ServerName: "testServer", Trace: true, Debug: true, JetStream: true, Port: 4222, StoreDir: "/tmp/js"}
+	ns, err = server.NewServer(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go ns.Start()
+	//ns.ConfigureLogger()
+	if !ns.ReadyForConnections(5 * time.Second) {
+		log.Fatal("Could not connect to embedded NATS server")
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module embedgo
 
-go 1.22.1
+go 1.23.0
+
+toolchain go1.23.11
+
+require (
+	github.com/nats-io/nats-server/v2 v2.11.7
+	github.com/nats-io/nats.go v1.43.0
+)
+
+require (
+	github.com/google/go-tpm v0.9.5 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/minio/highwayhash v1.0.3 // indirect
+	github.com/nats-io/jwt/v2 v2.7.4 // indirect
+	github.com/nats-io/nkeys v0.4.11 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.40.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
+)
