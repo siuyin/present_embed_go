4ad844218cd52824cbe9e4f345fdfa4d28fbf32b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-03 22:22:30 +0800

    refactor: stream code

--- 04_stream/main.go
+++ 04_stream/main.go
@@ -23,7 +23,7 @@ func init() {
 }
 
 func main() {
-	go procWorker()
+	procWorker()
 	http.HandleFunc("/proc", procHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -31,6 +31,17 @@ func main() {
 }
 
 func procWorker() {
+	wrkr := getWorker()
+
+	wrkr.Consume(func(m jetstream.Msg) {
+		fmt.Printf("procWorker: received: %s: ", m.Data())
+		time.Sleep(300 * time.Millisecond)
+		fmt.Printf("processed:  %s\n", m.Data())
+		m.Ack()
+	})
+
+}
+func getWorker() jetstream.Consumer {
 	nc, err := nats.Connect("", nats.InProcessServer(ns))
 	if err != nil {
 		log.Fatal("procWorker: Connect: ", err)
@@ -57,14 +68,7 @@ func procWorker() {
 		procStrm.DeleteConsumer(ctx, "wrkr")
 		log.Fatal("procWorker: create consumer: ", err)
 	}
-
-	wrkr.Consume(func(m jetstream.Msg) {
-		fmt.Printf("procWorker: received: %s: ", m.Data())
-		time.Sleep(300 * time.Millisecond)
-		fmt.Printf("processed:  %s\n", m.Data())
-		m.Ack()
-	})
-
+	return wrkr
 }
 
 func procHandler(w http.ResponseWriter, r *http.Request) {
@@ -79,15 +83,9 @@ func procHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	nc, err := nats.Connect("", nats.InProcessServer(ns))
+	js, err := getJetStream()
 	if err != nil {
-		log.Println(err)
 		return
-	}
-
-	js, err := jetstream.New(nc)
-	if err != nil {
-		log.Fatal("procHandler: new jetstream: ", err)
 	}
 
 	ctx := context.Background()
@@ -100,14 +98,26 @@ func procHandler(w http.ResponseWriter, r *http.Request) {
 
 	io.WriteString(w, fmt.Sprintf("Request ID: %s: %d processing requests queued.\n", id, nn))
 
-	nc.Flush()
-	nc.Close()
+	js.Conn().Flush()
+	js.Conn().Close()
 
 }
+func getJetStream() (jetstream.JetStream, error) {
 
+	nc, err := nats.Connect("", nats.InProcessServer(ns))
+	if err != nil {
+		return nil, err
+	}
+
+	js, err := jetstream.New(nc)
+	if err != nil {
+		return nil, err
+	}
+	return js, nil
+}
 func embedNATS() {
 	var err error
-	opts := &server.Options{ServerName: "testServer", Trace: true, Debug: true, JetStream: true, Port: 4222, StoreDir: "/tmp/js"}
+	opts := &server.Options{ServerName: "testServer", Trace: false, Debug: false, JetStream: true, Port: 4222, StoreDir: "/tmp/js"}
 	ns, err = server.NewServer(opts)
 	if err != nil {
 		log.Fatal(err)
